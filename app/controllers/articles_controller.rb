73941d7c36935728a26bb5ca15f71c8b6266439e
Amanda Suamy <anandasuamy@gmail.com>
2024-03-05 01:46:36 -0300

    Configuração das postagens e ajustes no bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,19 @@
 class ArticlesController < ApplicationController
+  before_action :set_artcile, only: %i[show edit update destroy]
+
   def index
-    @articles = Article.all
+    @highlights = Article.order(created_at: :desc).first(3)
+
+    current_page = (params[ :page] || 1).to_i
+    highlight_ids = @highlights.pluck(:id).join(',')
+
+    @articles = Article.order(created_at: :desc)
+    .where("id NOT IN(#{highlight_ids})")
+    .page(current_page).per(2)
+    
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,12 +31,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
     end
 
     def update
-      @article = Article.find(params[:id])
-
         if @article.update(article_params)
           redirect_to @article
         else
@@ -36,7 +42,6 @@ class ArticlesController < ApplicationController
       end 
 
       def destroy
-        @article = Article.find(params[:id])
         @article.destroy
 
         redirect_to root_path
@@ -49,4 +54,8 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :body)
     end
 
+    def set_artcile
+      @article = Article.find(params[:id])
+        
+      end
 end
